105dc3d73ffd8d3da8605dbe17c141b2007afe29
Arjunkumar P <p.arjunkumar@colanonline.com>
2024-05-20 20:31:22 +0530

    CREATIVE_FLAVOURS-PRODUCTION_SCREEN_NEWLY_ADDED

---
+++ MMS.Core/Entities/Production.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMS.Core.Entities
+{
+    [Table("production", Schema = "public")]
+    public class Production : BaseEntity
+        {
+            [Column("productionid")]
+            public int ProductionId { get; set; }
+
+            [Column("productiondate")]
+            public DateTime? ProductionDate { get; set; }
+
+            [Column("productioncode")]
+            public string ProductionCode { get; set; }
+
+            [Column("productionqty")]
+            public decimal ProductionQty { get; set; }
+
+            [Column("productionstatus")]
+            public int ProductionStatus { get; set; }
+
+            [Column("productid")]
+            public int ProductId { get; set; }
+
+            [Column("minqty")]
+            public decimal MinQty { get; set; }
+
+            [Column("maxqty")]
+            public decimal MaxQty { get; set; }
+
+            [Column("tempproductionqty")]
+            public decimal TempProductionQty { get; set; }
+
+            [Column("storecode")]
+            public int StoreCode { get; set; }
+
+            [Column("productionduedate")]
+            public DateTime? ProductionDueDate { get; set; }
+
+            [Column("productionfullfilldate")]
+            public DateTime? ProductionFullfillDate { get; set; }
+
+            [Column("refdocno")]
+            public decimal RefDocNo { get; set; }
+
+            [Column("refdocdate")]
+            public DateTime? RefDocDate { get; set; }
+
+            [Column("status1code")]
+            public string Status1Code { get; set; }
+
+            [Column("status1date")]
+            public DateTime? Status1Date { get; set; }
+
+            [Column("status1by")]
+            public string Status1By { get; set; }
+
+            [Column("status2code")]
+            public string Status2Code { get; set; }
+
+            [Column("status2date")]
+            public DateTime? Status2Date { get; set; }
+
+            [Column("status2by")]
+            public string Status2By { get; set; }
+
+            [Column("status3code")]
+            public string Status3Code { get; set; }
+
+            [Column("status3date")]
+            public DateTime? Status3Date { get; set; }
+
+            [Column("status3by")]
+            public string Status3By { get; set; }
+
+            [Column("createddate")]
+            public DateTime? CreatedDate { get; set; }
+
+            [Column("updateddate")]
+            public DateTime? UpdatedDate { get; set; }
+
+            [Column("deleteddate")]
+            public DateTime? DeletedDate { get; set; }
+
+            [Column("createdby")]
+            public string CreatedBy { get; set; }
+
+            [Column("updatedby")]
+            public string UpdatedBy { get; set; }
+
+            [Column("deletedby")]
+            public string DeletedBy { get; set; }
+        }
+
+    }
+
---
+++ MMS.Web/Controllers/ProductionController.cs
@@ -0,0 +1,33 @@
+﻿using MMS.Core.Entities;
+using MMS.Repository.Managers;
+using MMS.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MMS.Web.Controllers
+{
+    public class ProductionController : Controller
+    {
+        public ActionResult ProductionMaster()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ProductionGrid()
+        {
+            
+
+            return PartialView("partial/ProductionGrid");
+        }
+
+        [HttpGet]
+        public ActionResult ProductionDetails()
+        {
+            return View();
+        }
+
+    }
+}
