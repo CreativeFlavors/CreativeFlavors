f7690cafcea3e46d6761fc98f9d0ebf5757596c3
Arjunkumar P <p.arjunkumar@colanonline.com>
2024-07-19 10:25:48 +0530

    CREATIVEFLAVOURS_PO_SEARCHABLEPRODUCTS_RAISETOPO

---
+++ MMS.Core/Entities/ProductionMaterial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMS.Core.Entities
+{
+    public class ProductionMaterial :BaseEntity
+    {
+        [Key]
+        [Column("productionmaterialid")]
+        public int ProductionMaterialId { get; set; }
+
+        [Column("productid")]
+        public int ProductId { get; set; }
+
+        [Column("quantity")]
+        public decimal? Quantity { get; set; }
+
+        [Column("createdby")]
+        public string CreatedBy { get; set; }
+
+        [Column("updatedby")]
+        public string UpdatedBy { get; set; }
+
+        [Column("deletedby")]
+        public string DeletedBy { get; set; }
+
+        [Column("isactive")]
+        public bool IsActive { get; set; }
+
+        [Column("createddate")]
+        public DateTime? CreatedDate { get; set; }
+
+        [Column("updateddate")]
+        public DateTime? UpdatedDate { get; set; }
+
+        [Column("deleteddate")]
+        public DateTime? DeletedDate { get; set; }
+    }
+}
---
+++ MMS.Repository/Managers/ProductionMaterialManager.cs
@@ -0,0 +1,31 @@
+﻿using MMS.Core.Entities;
+using MMS.Data;
+using MMS.Data.Context;
+using MMS.Repository.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMS.Repository.Managers
+{
+    public class ProductionMaterialManager : IProductionMaterialService,IDisposable
+    {
+        private readonly UnitOfWork unitOfWork = new UnitOfWork();
+        private Repository<ProductionMaterial> productionmaterialrepository;
+        public ProductionMaterialManager()
+        {
+            productionmaterialrepository= unitOfWork.Repository<ProductionMaterial>();
+
+
+        }
+        public void Dispose()
+        {
+            using (MMSContext context = new MMSContext())
+            {
+                context.Dispose();
+            }
+        }
+    }
+}
