6943497d522693d8dc5c9d239b343a5ebe611fc2
Arjunkumar P <p.arjunkumar@colanonline.com>
2024-07-26 15:28:52 +0530

    CREATIVEFLAVOURS_BATCHSTOCK_PUTMETHODADDED

--- MMS.Repository/Managers/BatchStockManager.cs
+++ MMS.Repository/Managers/BatchStockManager.cs
@@ -43,11 +43,82 @@ namespace MMS.Repository.Managers
             }
             return salesorder;
         }
+        public bool Put(BatchStock arg)
+        {
+            bool result = false;
+            try
+            {
+                BatchStock model = BatchStockrep.Table.Where(f => f.BatchStockId == arg.BatchStockId).FirstOrDefault();
+                if (model != null)
+                {
+                    model.BatchStockId = arg.BatchStockId;
+                    model.SupplierId = arg.SupplierId;
+                    model.StoreCode = arg.StoreCode;
+                    model.productid = arg.productid;
+                    model.BatchCode = arg.BatchCode;
+                    model.AltBatchCode = arg.AltBatchCode;
+                    model.ExpiryDate = arg.ExpiryDate;
+                    model.Quantity = arg.Quantity;
+                    model.GrnNumber = arg.GrnNumber;
+                    model.GrnDate = arg.GrnDate;
+                    model.GrnDetailId = arg.GrnDetailId;
+                    model.Price = arg.Price;
+                    model.Cost = arg.Cost;
+                    model.TaxCode = arg.TaxCode;
+                    model.UomId = arg.UomId;
+                    model.ReservedQty = arg.ReservedQty;
+                    model.LastTransMode = arg.LastTransMode;
+                    model.LastTransNumber = arg.LastTransNumber;
+                    model.LastTransDate = arg.LastTransDate;
+                    model.LastTransQty = arg.LastTransQty;
+                    model.status = arg.status;
+                    model.producttype = arg.producttype;
+                    model.ProductCode = arg.ProductCode;
+                    model.UpdatedDate = DateTime.Now;
+                    //model.CreatedBy = "";
+                    //string username = HttpContext.Current.Session["UserName"]?.ToString();
+                    //if (username != null)
+                    //{
+                    //    model.UpdatedBy = username;
+                    //}
+                    model.UpdatedBy = "Admin";
+                    BatchStockrep.Update(model);
+                    result = true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ex.Message.ToString(), this.GetType().Name, System.Reflection.MethodBase.GetCurrentMethod().Name);
+                result = false;
+            }
+
+            return result;
+        }
         public List<FineshedgoodsReport> Fineshedgoods_Report()
         {
             List<FineshedgoodsReport> FineshedgoodsReport = new List<FineshedgoodsReport>();
             FineshedgoodsReport = BatchStockrep.Fineshedgoods();
             return FineshedgoodsReport;
+        }
+        public BatchStock GetByProductCode(string productcode)
+        {
+            BatchStock finishedgoodlist = new BatchStock();
+            if (productcode != null)
+            {
+                try
+                {
+                    finishedgoodlist = BatchStockrep.Table.Where(x => x.ProductCode == productcode).FirstOrDefault();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+            return finishedgoodlist;
         }
 
         public List<BatchStock> Get()
